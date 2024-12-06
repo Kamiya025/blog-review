353e1c537606e5809ba4eb5d44f22b64098559a4
Kamiya025 <hawk01525@gmail.com>
2024-12-06 16:42:09 +0700

    "Moved `className` from `div` to `main` and swapped `main` and `div` elements in `MainLayout` component."

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -33,19 +33,19 @@ export default function MainLayout({
         <Suspense fallback={<SkeletonLayout />}>
           <AuthProvider>
             <LayoutHeader />
-            <div className="flex flex-col h-full gap-1 font-[family-name:var(--font-geist-sans)]">
-              <main
+            <main className="flex flex-col h-full gap-1 font-[family-name:var(--font-geist-sans)]">
+              <div
                 id={"body-main"}
                 className="grow flex flex-col w-full relative"
               >
                 <div className="max-w-screen-xl p-3 lg:p-8 w-full overflow-y-auto grow mx-auto">
                   {children}
                 </div>
-              </main>
+              </div>
               <footer className="max-h-8 flex gap-6 flex-wrap items-center justify-center px-2 py-1 bg-orange-400 w-full text-white">
                 @Design by Kamiya
               </footer>
-            </div>
+            </main>
           </AuthProvider>
         </Suspense>
       </body>
